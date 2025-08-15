f8c66f0d5879939ad982005a269aeae1f38fcb8f
CyberEzpertz <skyblaze24@gmail.com>
2025-08-16 02:26:58 +0800

    feat: add back navbar for individual clearance form

--- src/app/services/individual-clearance/page.tsx
+++ src/app/services/individual-clearance/page.tsx
@@ -1,9 +1,13 @@
+import BackNavBar from "@/components/back-nav-bar";
 import { IndividualClearanceForm } from "./_components/individual-clearance-form";
 
 export default function ServicesPage() {
   return (
-    <div className="p-4">
-      <IndividualClearanceForm />
+    <div>
+      <BackNavBar title="Individual Clearance" />
+      <div className="p-4">
+        <IndividualClearanceForm />
+      </div>
     </div>
   );
 }
