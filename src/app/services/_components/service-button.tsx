af2cb3bbc0a906f63eba5a1778d0d11f47cb557f
CyberEzpertz <skyblaze24@gmail.com>
2025-08-16 02:25:27 +0800

    feat: add links to services in service page

---
+++ src/app/services/_components/service-button.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+interface ServiceButtonProps {
+  title: string;
+  href: string;
+}
+export default function ServiceButton({ title, href }: ServiceButtonProps) {
+  const router = useRouter();
+
+  return (
+    <div
+      className="bg-primary text-primary-foreground flex h-20 flex-row items-center rounded-xl p-4 font-semibold"
+      onClick={() => router.push(href)}
+    >
+      {title}
+      <div className="bg-secondary text-secondary-foreground rounded-full p-2">
+        <ArrowRight className="size-4" />
+      </div>
+    </div>
+  );
+}
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,41 @@
 import BackNavBar from "@/components/back-nav-bar";
+import ServiceButton from "./_components/service-button";
 
 interface ServicesPageProps {}
+
+type pageLink = {
+  title: string;
+  link: string;
+};
+
+const pageLinks: pageLink[] = [
+  {
+    title: "Business Clearance",
+    link: "/services/business-clearance",
+  },
+  {
+    title: "Individual Clearance",
+    link: "/services/individual-clearance",
+  },
+  {
+    title: "Certificate of No Objection",
+    link: "/services/no-objection",
+  },
+  {
+    title: "Certificate of Residency",
+    link: "/services/residency",
+  },
+];
+
 export default function ServicesPage({}: ServicesPageProps) {
   return (
     <div>
       <BackNavBar title="Digital Services" />
+      <div className="grid grid-cols-2 gap-2 p-4">
+        {pageLinks.map(({ title, link }, index) => (
+          <ServiceButton key={index} title={title} href={link} />
+        ))}
+      </div>
     </div>
   );
 }
