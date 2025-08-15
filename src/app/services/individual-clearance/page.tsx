418af35bdbf9676ddb8830b37985852a31f109fc
CyberEzpertz <skyblaze24@gmail.com>
2025-08-16 00:48:25 +0800

    feat: implement multi-step form for individual clearance forms

---
+++ src/app/services/individual-clearance/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { IndividualClearanceForm } from "./_components/individual-clearance-form";
+
+export default function ServicesPage() {
+  return (
+    <div className="p-4">
+      <IndividualClearanceForm />
+    </div>
+  );
+}
