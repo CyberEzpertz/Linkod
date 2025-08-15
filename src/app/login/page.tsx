caf13f61a5268299ffa79f45f90297f49112d0c6
CyberEzpertz <skyblaze24@gmail.com>
2025-08-16 06:48:58 +0800

    feat: improve register and login pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { Button } from "@/components/ui/button";
-import { Form } from "@/components/ui/form";
 import { FormTextField } from "@/components/forms/form-text-field";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import LinkodLogo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 const formSchema = z.object({
   email: z.string().email("Invalid email address"),
@@ -32,41 +33,48 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="container mx-auto flex min-h-[calc(100vh-1rem)] items-center justify-center p-4 sm:p-6 lg:p-8">
-      <Card className="mx-auto w-full max-w-md">
-        <CardHeader className="space-y-1 px-6 py-4">
-          <CardTitle className="text-center text-2xl">Login</CardTitle>
-        </CardHeader>
-        <CardContent className="px-6 pb-6">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormTextField
-                form={form}
-                formKey="email"
-                label="Email"
-                placeholder="Enter your email"
-                inputType="email"
-              />
-              <FormTextField
-                form={form}
-                formKey="password"
-                label="Password"
-                placeholder="Enter your password"
-                inputType="password"
-              />
-              <Button type="submit" className="mt-6 w-full">
-                Login
-              </Button>
-              <div className="mt-4 text-center text-sm">
-                {"Don't have an account? "}
-                <Link href="/register" className="text-primary hover:underline">
-                  Register here
-                </Link>
-              </div>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
+    <div className="to-secondary/50 relative bg-linear-to-b">
+      <div className="bg-secondary absolute -z-10 h-32 w-full rounded-b-[80%]" />
+      <div className="container mx-auto flex min-h-[calc(100vh-1rem)] flex-col items-center justify-center p-4 sm:p-6 lg:p-8">
+        <LinkodLogo className="mb-8" />
+        <Card className="mx-auto w-full max-w-md">
+          <CardContent className="px-6 py-2">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <FormTextField
+                  form={form}
+                  formKey="email"
+                  label="Email"
+                  placeholder="Enter your email"
+                  inputType="email"
+                />
+                <FormTextField
+                  form={form}
+                  formKey="password"
+                  label="Password"
+                  placeholder="Enter your password"
+                  inputType="password"
+                />
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+                <div className="text-center text-sm">
+                  {"Don't have an account? "}
+                  <Link
+                    href="/register"
+                    className="text-primary hover:underline"
+                  >
+                    Register here
+                  </Link>
+                </div>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
